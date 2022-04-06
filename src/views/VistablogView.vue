454cc905e9e33ec73a4911220a275ae14f0950fb
a8dogdark <a8dogdark@gmail.com>
2022-04-06 03:59:42 -0400

    06-04-2022

--- src/views/VistablogView.vue
+++ src/views/VistablogView.vue
@@ -1,10 +1,22 @@
 <template>
-    <div>
-        desde la vista de articulos
+    <div class="mt-o">
+        <Menu/>
+        <div class="container">
+            <div class="row">
+                <div class="col-12"><h2 class="text-uppercase">titulo</h2><hr/></div>
+                <div class="col-12 col-md-8">desde la vista de articulos</div>
+                <div class="col-12 col-md-4">aside</div>
+            </div>
+        </div>
     </div>
 </template>
 <script>
+import Menu from '../components/MenuComponents.vue'
+
 export default {
-    name:'VistablogView'
+    name:'VistablogView',
+    components:{
+        Menu
+    }
 }
 </script>
